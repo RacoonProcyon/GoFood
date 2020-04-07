c79ccc653155faea20d20ce729c7fc067c7788bf
Vaibhav Nadgonde <itgenesys@gmail.com>
2020-04-08 03:02:42 +0530

    Updates

---
+++ precache-manifest.ce2db04173bbeba79fe23db5f8600bc2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd7fd47865a002101dac7b3525126cf9",
+    "url": "/GoFood/index.html"
+  },
+  {
+    "revision": "7fbb92a9b142be1c7b15",
+    "url": "/GoFood/static/css/main.79d1b22d.chunk.css"
+  },
+  {
+    "revision": "55b2c3a815156ac87ddf",
+    "url": "/GoFood/static/js/2.8d5ca72d.chunk.js"
+  },
+  {
+    "revision": "eb56b19eba63a53416f4fd778023529f",
+    "url": "/GoFood/static/js/2.8d5ca72d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fbb92a9b142be1c7b15",
+    "url": "/GoFood/static/js/main.b2077d0a.chunk.js"
+  },
+  {
+    "revision": "b6e3c1c89a6ae35a40c9",
+    "url": "/GoFood/static/js/runtime-main.b48b38ff.js"
+  },
+  {
+    "revision": "50e5b8df1c3cea56673561add5f77251",
+    "url": "/GoFood/static/media/332-3324201_gofood-logo-png-logo-go-food-vector.50e5b8df.png"
+  },
+  {
+    "revision": "10bf459a56574da37914ce88139163d1",
+    "url": "/GoFood/static/media/Foods_(cropped) (1).10bf459a.jpg"
+  }
+]);
